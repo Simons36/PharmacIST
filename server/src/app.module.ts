11f6170917b66a57a0740f9666136917089e13eb
Simão Silva <email@simaosilva.com>
2024-04-29 10:34:23 +0100

    Refactored app structure

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
+import { AppConfigModule } from './config/app-config.module';
+import { DatabaseModule } from './database/database.module';
+import { AppConfigService } from './config/app-config.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UserModule, AuthModule, AppConfigModule, DatabaseModule],
+  controllers: [AuthController],
+  providers: [AuthService, AppConfigService],
 })
 export class AppModule {}
