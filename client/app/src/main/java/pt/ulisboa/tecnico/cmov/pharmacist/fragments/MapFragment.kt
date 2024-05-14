e796f614e1b9113be89565c193a5335ff6844326
Simão Silva <email@simaosilva.com>
2024-05-14 10:42:49 +0100

    Added updating current location marker when location changes

--- client/app/src/main/java/pt/ulisboa/tecnico/cmov/pharmacist/fragments/MapFragment.kt
+++ client/app/src/main/java/pt/ulisboa/tecnico/cmov/pharmacist/fragments/MapFragment.kt
@@ -1,9 +1,12 @@
 package pt.ulisboa.tecnico.cmov.pharmacist.fragments
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -18,17 +21,25 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.tasks.Task
 import pt.ulisboa.tecnico.cmov.pharmacist.R
 import pt.ulisboa.tecnico.cmov.pharmacist.util.UtilFunctions
 
-class MapFragment : Fragment(), OnMapReadyCallback {
+class MapFragment : Fragment(), OnMapReadyCallback, LocationListener {
+
+    private val MIN_TIME = 60000L; //60 seconds for updating location
+    private val MIN_DISTANCE = 90f; //1000 meters for updating location
 
     private val FINE_PERMISSION_CODE = 1
     private lateinit var googleMap: GoogleMap
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private lateinit var lastKnownLocation: Location
+    private var currentLocationMarker : Marker? = null
+
+    // For updating location when the user moves
+    private  lateinit var locationManager : LocationManager
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,6 +55,36 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         Log.i("MapFragment", "MapFragment DEBUG")
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+
+        // Initialize locationManager using getSystemService
+        locationManager = requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+
+        if (ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireActivity(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                FINE_PERMISSION_CODE
+            )
+
+            onRequestPermissionsResult(FINE_PERMISSION_CODE, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), intArrayOf(PackageManager.PERMISSION_GRANTED))
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this)
 
         return view
     }
@@ -62,7 +103,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             if (location != null) {
                 lastKnownLocation = location
                 addMarkerToCurrentLocation()
-                moveMarkerToLocation(lastKnownLocation)
+                moveCameraToLocation(lastKnownLocation)
 
             }
         }
@@ -82,8 +123,7 @@ class MapFragment : Fragment(), OnMapReadyCallback {
             return null;
         }
 
-        val task: Task<Location> = fusedLocationProviderClient.lastLocation
-        return task;
+        return fusedLocationProviderClient.lastLocation
     }
 
     private fun addMarkerToCurrentLocation() {
@@ -97,15 +137,15 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         // Create a BitmapDescriptor from the resized bitmap
         val icon = BitmapDescriptorFactory.fromBitmap(scaledBitmap)
 
-        googleMap.addMarker(
+        this.currentLocationMarker = googleMap.addMarker(
             MarkerOptions()
                 .position(LatLng(lastKnownLocation.latitude, lastKnownLocation.longitude))
-                .title("Current Location")
                 .icon(icon)
+                .title("Current Location")
         )
     }
 
-    private fun moveMarkerToLocation(location: Location){
+    private fun moveCameraToLocation(location: Location){
         val latLng = LatLng(location.latitude, location.longitude)
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f))
     }
@@ -124,5 +164,11 @@ class MapFragment : Fragment(), OnMapReadyCallback {
                 Toast.makeText(requireContext(), "Location permission denied", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    override fun onLocationChanged(location: Location) {
+        lastKnownLocation = location
+        currentLocationMarker?.position = LatLng(location.latitude, location.longitude)
+        moveCameraToLocation(lastKnownLocation)
     }
 }
